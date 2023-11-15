90b66fc953a3aa8594cb9dd1b01b34c97d0a9ba4
Roman Gudz <alien31roman@gmail.com>
2023-11-15 18:32:38 +0300

    first commit

---
+++ class_01/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const nameProduct = 'Iphone 15';
+const quantityProducts = 10;
+const categoryProduct = 'Phone';
+const priceProduct = 1500;
+
+console.log('nameProduct: ', nameProduct);
+
+console.log('Сумма всех товаров на складе = ', quantityProducts * priceProduct);
