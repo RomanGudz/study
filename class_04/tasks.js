b7239d237cce16306f18eb451a47b4379d90a8d7
Roman Gudz <alien31roman@gmail.com>
2023-11-17 00:11:27 +0300

    add hardTask

--- class_04/tasks.js
+++ class_04/tasks.js
@@ -1,30 +1,30 @@
-// 'use strict'
+'use strict'
 
-// {
-//   const rain = Math.round((Math.random() * 1));
-//   const res = rain > 0 ? 'Пошёл дождь. Возьмите зонт!' : 'Дождя нет!';
+{
+  const rain = Math.round((Math.random() * 1));
+  const res = rain > 0 ? 'Пошёл дождь. Возьмите зонт!' : 'Дождя нет!';
 
-//   console.log('rain: ', rain);
-//   console.log('res: ', res);
-// }
+  console.log('rain: ', rain);
+  console.log('res: ', res);
+}
 
-// {
-//   const minPointsExam = 264;
-//   const examMath = Number(prompt('Введите кол-во баллов по математике', ''));
-//   const examRussian = Number(prompt('Введите кол-во баллов по русскому языку', ''));
-//   const examIt = Number(prompt('Введите кол-во баллов по информатике', ''));
-//   const resPointsExam = (examMath + examRussian + examIt) > minPointsExam ? 'Поздравляю, вы поступили на бюджет!' : 'Нужно еще поучиться';
+{
+  const minPointsExam = 264;
+  const examMath = Number(prompt('Введите кол-во баллов по математике', ''));
+  const examRussian = Number(prompt('Введите кол-во баллов по русскому языку', ''));
+  const examIt = Number(prompt('Введите кол-во баллов по информатике', ''));
+  const resPointsExam = (examMath + examRussian + examIt) > minPointsExam ? 'Поздравляю, вы поступили на бюджет!' : 'Нужно еще поучиться';
 
-//   console.log('resPointsExam: ', resPointsExam);
-// }
+  console.log('resPointsExam: ', resPointsExam);
+}
 
-// {
-//   const minMoney = 100;
-//   const howMoney = Number(prompt('Введите сумму для снятия наличных', ''));
+{
+  const minMoney = 100;
+  const howMoney = Number(prompt('Введите сумму для снятия наличных', ''));
 
-//   if (howMoney % minMoney !== 0) {
-//     console.log('Сумма должна быть кратна 100р')
-//   } else {
-//     console.log('Будет выдына сумма ' + howMoney)
-//   }
-// }
+  if (howMoney % minMoney !== 0) {
+    console.log('Сумма должна быть кратна 100р')
+  } else {
+    console.log('Будет выдына сумма ' + howMoney)
+  }
+}
