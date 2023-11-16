2a01eaa09ba34d62fcfa3eae7e805cf5ee081d17
Roman Gudz <alien31roman@gmail.com>
2023-11-16 23:31:43 +0300

    add class_04

---
+++ class_04/index.js
@@ -0,0 +1,47 @@
+'use strict';
+
+{
+  const nameProduct = 'Iphone 15';
+  const quantityProducts = 10;
+  const categoryProduct = 'Phone';
+  const priceProduct = 1500;
+
+  // console.log('nameProduct: ', nameProduct);
+
+  // console.log('Сумма всех товаров на складе = ', quantityProducts * priceProduct);
+}
+
+{
+  const nameProduct = 'Laptop MSI';
+  const quantityProducts = 5;
+  const categoryProduct = 'Laptop';
+  const priceProduct = 2000;
+
+  // console.log('nameProduct: ', nameProduct);
+  //   console.log('Сумма всех товаров на складе = ', quantityProducts * priceProduct);
+}
+
+{
+  // const requestProduct = prompt('Укажите наименование товара', '');
+  // const requestQuantityProducts = Number(prompt('Укажите количество товара', ''));
+  // const requestCategoryProduct = prompt('Укажите категорию товара', '');
+  // const requestPriceProduct = Number(prompt('Укажите цену товара', ''));
+  // const sumProducts = requestPriceProduct * requestQuantityProducts;
+  // const date = new Date().toLocaleDateString();
+
+
+  // console.log(`Состояние склада товара на дату: ${date}`);
+  // console.log('Категория товара:', requestCategoryProduct + '|' + typeof requestCategoryProduct);
+  // console.log('Наименование товара:', requestProduct + '|' + typeof requestProduct);
+  // if (requestQuantityProducts > 0) {
+  //   console.log('Количество:', requestQuantityProducts + '|' + typeof requestQuantityProducts);
+  // } else {
+  //   console.log('Вы ввели некорректные данные')
+  // };
+  // if (requestPriceProduct > 0) {
+  //   console.log('Товаров на сумму:', sumProducts + '|' + typeof sumProducts);
+  // } else {
+  //   console.log('Вы ввели некорректные данные')
+  // }
+
+}
