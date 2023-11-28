26afc92190b1dceffaecdf4f9ed336da2b22b7d5
Roman Gudz <alien31roman@gmail.com>
2023-11-28 13:35:59 +0300

    Add ESLint and fix error class_10

--- class_10/task_01.js
+++ class_10/task_01.js
@@ -1,31 +1,40 @@
-'use strict'
+'use strict';
 
 const cart = {
   items: [],
-  get totalPrice() { return this.calculateItemPrice() },
+  get totalPrice() {
+    return this.calculateItemPrice();
+  },
   count: 0,
   add(product, price, count = 1) {
-    cart.items.push({ productName: product, price: price, count: count });
+    cart.items.push({productName: product, price, count});
     cart.increaseCount(count);
     cart.calculateItemPrice();
   },
-  increaseCount(num) { this.count += num },
+  increaseCount(num) {
+    this.count += num;
+  },
   calculateItemPrice() {
-    const sumTotal = cart.items.reduce(function (acc, item) {
+    const sumTotal = cart.items.reduce((acc, item) => {
       const sum = acc + (item.price * item.count);
       return sum;
     }, 0);
-    return sumTotal
+    return sumTotal;
   },
-  clear() { this.items = []; this.totalPrice = 0; this.count = 0; },
-  print() { console.log(JSON.stringify(this.items)), console.log(JSON.stringify(this.totalPrice)); },
+  clear() {
+    this.items = []; this.totalPrice = 0; this.count = 0;
+  },
+  print() {
+    console.log(JSON.stringify(this.items)),
+    console.log(JSON.stringify(this.totalPrice));
+  },
 };
 
-cart.add('phone', 1500, 4)
-cart.add('phone', 150, 4)
-cart.add('phone', 100, 4)
+cart.add('phone', 1500, 4);
+cart.add('phone', 150, 4);
+cart.add('phone', 100, 4);
 
 console.log(cart.items);
 
 console.log(cart.totalPrice);
-cart.print()
+cart.print();
